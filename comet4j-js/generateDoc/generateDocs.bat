7cff44664d6f354c45bab27297845a849d247874
jinghai <jinghai.xiao@gmail.com>
2014-04-08 15:49:42 +0800

    乱码问题

--- comet4j-js/generateDoc/generateDocs.bat
+++ comet4j-js/generateDoc/generateDocs.bat
@@ -2,6 +2,6 @@ set extDocJarFile="ext-doc.jar"
 set extDocXMLFile="comet4j-js-doc.xml"  
 set extDocTemplate="template/ext/template.xml"  
 set outputFolder="../docs"  
-java -jar %extDocJarFile% -p %extDocXMLFile% -o %outputFolder% -t %extDocTemplate%  -verbose
+java -jar -Dfile.encoding=UTF-8 %extDocJarFile% -p %extDocXMLFile% -o %outputFolder% -t %extDocTemplate%  -verbose
 
 echo. & pause
